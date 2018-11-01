b3c89613fd32e332e63ad3448ebfc9c897e576e9
snailQQ <>
2018-10-31 22:41:42 -0700

    deleted a duplicated file added the 2nd solution to Q21

--- src/leetcode/leetcode/to40/_21MergeTwoSortedLists.java
+++ src/leetcode/leetcode/to40/_21MergeTwoSortedLists.java
@@ -52,4 +52,44 @@ public class _21MergeTwoSortedLists {
         }
         return dummy.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 == null) {
+            return null;
+        }
+
+        if(l1 == null) {
+            return l2;
+        }
+
+        if(l2 == null) {
+            return l1;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while(l1 != null & l2 != null) {
+            int value = 0;
+            if(l1.val <= l2.val) {
+                value = l1.val;
+                l1 = l1.next;
+            } else {
+                value = l2.val;
+                l2 = l2.next;
+            }
+            ListNode newNode = new ListNode(value);
+            p.next = newNode;
+            p = p.next;
+
+        }
+
+        if(l1 != null) {
+            p.next = l1;
+        }
+
+        if(l2 != null) {
+            p.next = l2;
+        }
+        return dummy.next;
+    }
 }
